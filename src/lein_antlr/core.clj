6cf5bed0cd1cd1a5050465fec3051b2caff88ff3
Scott Haney <emailalmondbranch@gmail.com>
2017-11-07 01:11:24 +0000

    Get lein run working and import antlr into the code

--- src/lein_antlr/core.clj
+++ src/lein_antlr/core.clj
@@ -1,6 +1,12 @@
-(ns lein-antlr.core)
+(ns lein-antlr.core
+ (:import
+   [org.antlr.v4 Tool]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main
+ [& args]
+ (foo "Test"))
