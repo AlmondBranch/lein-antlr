a1ba3a5a43f0312220881fc8ac6e29141df2ff56
Scott Haney <emailalmondbranch@gmail.com>
2017-11-08 02:50:48 +0000

    Add rudimentary function for splitting simple command line arguments which isn't production ready

--- src/lein_antlr/core.clj
+++ src/lein_antlr/core.clj
@@ -1,10 +1,17 @@
 (ns lein-antlr.core
  (:import
-   [org.antlr.v4 Tool]))
+   [org.antlr.v4 Tool])
+ (:use [clojure.string :only (split)])
+)
 
 (defn antlr
  [file]
  (Tool/main (into-array String [file]))
+)
+
+(defn split-args
+ [args]
+ (split args #" ")
 )
 
 (defn -main
