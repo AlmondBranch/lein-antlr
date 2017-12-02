53f3e295db7b2c75234a9792e8ea58fdef97e31d
Scott Haney <emailalmondbranch@gmail.com>
2017-12-02 00:49:56 +0000

    Remove helper methods in antlr.clj since they are now basic enough to include in the main plugin method

--- src/leiningen/antlr.clj
+++ src/leiningen/antlr.clj
@@ -2,23 +2,12 @@
  (:import
    [org.antlr.v4 Tool]
    [com.github.almondbranch.command_line_parser CmdArgsReader])
- (:use [clojure.string :only (split)])
-)
-
-(defn antlr-tool
- [args]
- (Tool/main (into-array String args))
-)
-
-(defn split-args
- [args]
- (CmdArgsReader/read args)
 )
 
 (defn antlr
  "Compiles antlr grammars"
  [project & args]
   (-> (get project :antlr-cmd)
-     split-args
-     antlr-tool)
+     CmdArgsReader/read
+     Tool/main)
 )
