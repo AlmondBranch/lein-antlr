ccba04a44bac5383da1152078f2ca4cb1513111d
Scott Haney <emailalmondbranch@gmail.com>
2017-12-02 01:01:32 +0000

    Update the description, url, and license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lein-antlr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Lein plugin adding a task for running antlr4"
+  :url "https://github.com/AlmondBranch/lein-antlr"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :eval-in-leiningen true
   :dependencies [[org.antlr/antlr4 "4.7"]
                  [com.github.almondbranch/command-line-parser "1.2.0"]]
